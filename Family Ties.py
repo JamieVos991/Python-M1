0730725d80ad0006942d776371d0636053276418
Jamie <31694@ma-web.nl>
2020-09-18 12:42:42 +0200

    Family Ties

---
+++ Family Ties.py
@@ -0,0 +1,8 @@
+
+Naam = 'Jamie'
+Leeftijd = 16
+Plaats = 'Callantsoog' 
+Afstand = 68 
+
+print("Mijn naam is " + Naam + " ik ben " + str(Leeftijd) + " jaar oud en woon in " + Plaats + ". Ik woon " + str(Afstand) + " kilometer van het Mediacollege. ")
+print(type(Plaats))
