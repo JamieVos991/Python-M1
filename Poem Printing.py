9b720a558024a2d71a1815c5d31cb698f1d5a714
Jamie <31694@ma-web.nl>
2020-09-25 12:30:11 +0200

    Poem Printing

---
+++ Poem Printing.py
@@ -0,0 +1,10 @@
+vers1 = "Ik woon in Callantsoog waar ik knipoog. Mijn naam is Jamie Vos en ik houc van doritos. Ik ben nieuw in Amsterdam, dronk iets te veel en toen was ik lam. Ik doe de opleding Software Development daar verdien ik later een paar cent."
+vers2 ="Ik kijk vaak Netflix en daarom houd ik van pannekoekenmix. Ik heb een zus en heb niet het corona virus."
+
+print("")
+
+print(vers1)
+
+print("")
+
+print(vers2) 
