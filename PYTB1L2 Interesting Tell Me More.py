ecab002c53b85a3bb68067aba9b3f2b5fe29f84a
Jamie <31694@ma-web.nl>
2020-10-28 21:33:40 +0100

    Commits

---
+++ PYTB1L2 Interesting Tell Me More.py
@@ -0,0 +1,14 @@
+
+voornaam = input("Wat is je naam? ")
+
+print("")
+
+leeftijd = input("Hoe oud ben je? ")
+
+print("")
+
+plaats = input("Waar woon je? ")
+
+print("")
+
+print("Hallo" , voornaam + ',' , "Je bent" , leeftijd , "Jaar jong en je woont in" , plaats + "," " Leuk!")
