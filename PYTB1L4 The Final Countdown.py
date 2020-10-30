71d9e1550e90be350c18b81d0495a72b8caaceaa
Jamie <31694@ma-web.nl>
2020-10-30 13:38:02 +0100

    Commits

--- PYTB1L4 The Final Countdown.py
+++ PYTB1L4 The Final Countdown.py
@@ -1,11 +1,12 @@
 import time 
 
-nummer = 1000
+nummer = 10
 
-while nummer <= 1000:
+while nummer > 0:
     print(nummer)
     nummer = nummer -1
     time.sleep(1)
     
-    if nummer == 0:
-        print("Dit is speciaal :) ")
+  
+print("Je hebt je tijd verspild :) ")
+
---
+++ PYTB1L6 Can You Handle The List.py
@@ -0,0 +1,23 @@
+import time
+
+lijst = [True, int(1), float(2), int(3), float(4)]
+
+print(lijst)
+
+lijst.remove(2)
+
+time.sleep(1)
+print("")
+print(lijst)
+
+lijst.append(5)
+
+time.sleep(1)
+print("")
+print(lijst)
+
+lijst.clear()
+
+time.sleep(1)
+print("")
+print(lijst)
